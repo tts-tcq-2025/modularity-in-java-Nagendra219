e8e00610ad8c9f9ccd63fde89e2b11d559d27dfa
Nagendra219 <76140851+Nagendra219@users.noreply.github.com>
2025-08-12 09:56:25 +0530

    Create MajorColor.java

---
+++ colorcoder/MajorColor.java
@@ -0,0 +1,16 @@
+import ColorCode;
+public enum MajorColor implements ColorCode {
+        WHITE(0),
+        RED(1),
+        BLACK(2),
+        YELLOW(3),
+        VIOLET(4);
+        private int index;
+        MajorColor(int index) {
+            this.index = index;
+        }
+        @Override
+        public int getIndex() {
+            return index;
+        }
+    }
