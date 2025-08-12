9610c4a0a5f1c39bd6a34c4314c9aeb97c03ad08
Nagendra219 <76140851+Nagendra219@users.noreply.github.com>
2025-08-12 10:02:30 +0530

    Update ColorCodeService.java

--- colorcoder/ColorCodeService.java
+++ colorcoder/ColorCodeService.java
@@ -16,3 +16,31 @@
             }
             return null;
         }
+
+
+ public static ColorPair getColorFromPairNumber(int pairNumber) {
+            int zeroBasedPairNumber = pairNumber - 1;
+            MajorColor majorColor = fromIndex(MajorColor.class, zeroBasedPairNumber / numberOfMinorColors);
+            MinorColor minorColor = fromIndex(MinorColor.class, zeroBasedPairNumber % numberOfMinorColors);
+            return new ColorPair(majorColor, minorColor);
+        }
+
+        public static int getPairNumberFromColor(MajorColor major, MinorColor minor) {
+            return major.getIndex() * numberOfMinorColors + minor.getIndex() + 1;
+        }
+        
+        public static List<String> getColorCodingManual() {
+            List<String> manual = new ArrayList<>();
+            for (MajorColor major : MajorColor.values()) {
+                for (MinorColor minor : MinorColor.values()) {
+                    int pairNumber = getPairNumberFromColor(major, minor);
+                    String pairString = String.format("Pair %2d: %s, %s",
+                        pairNumber,
+                        MajorColorNames[major.getIndex()],
+                        MinorColorNames[minor.getIndex()]);
+                    manual.add(pairString);
+                }
+            }
+            return manual;
+        }
+    }
